ca4193dbf0915d34ffdf9b3b10f4caba94203c8b
monika-m <mashiraku-monika@dmm.com>
2017-04-28 15:59:55 +0900

    testing onclick event

--- app/src/main/java/mnk/mnktestapp/DisplayListActivity.java
+++ app/src/main/java/mnk/mnktestapp/DisplayListActivity.java
@@ -1,15 +1,20 @@
 package mnk.mnktestapp;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class DisplayListActivity extends AppCompatActivity {
+public class DisplayListActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
 
+    private String[] messagesList;
+    private int tappedPosition = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +22,30 @@ public class DisplayListActivity extends AppCompatActivity {
 
         // retrieve the type of list to be shown
         Intent intent = getIntent();
-        String[] messagesList = intent.getStringArrayExtra(MainActivity.LIST_VALUES_TYPE);
+        messagesList = intent.getStringArrayExtra(MainActivity.LIST_VALUES_TYPE);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, messagesList);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+        listView.setOnItemLongClickListener(this);
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        String item = messagesList[position];
+        setPosition(position);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(item);
+        alert.show();
+        return true;
+    }
+
+    public void setPosition(int position) {
+        tappedPosition = position;
+    }
+
+    public int getPosition() {
+        return tappedPosition;
     }
 }
